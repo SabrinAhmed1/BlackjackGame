9bff321efe3569a70529b5e087941246824f792d
SabrinAhmed1 <sabrin.ahmed030306@gmail.com>
2024-12-14 17:33:14 -0600

    Implemented Suit enum to represents the Suits for the cards

--- BlackjackGame/src/blackjackgame/BlackjackGame.java
+++ BlackjackGame/src/blackjackgame/BlackjackGame.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package blackjackgame;
 
 /**
- *
- * @author sabri
+ * A simple one-player Blackjack Game
+ * @author Sabrin Ahmed
  */
 public class BlackjackGame {
 
@@ -14,7 +10,7 @@ public class BlackjackGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
---
+++ BlackjackGame/src/blackjackgame/Suit.java
@@ -0,0 +1,32 @@
+package blackjackgame;
+
+/**
+ * An enumeration representing the different suits in a card deck.
+ * @author sabri
+ */
+public enum Suit {
+    /** Represents Clubs Suit*/
+    CLUBS('C'),
+    /** Represents Diamonds Suit*/
+    DIAMONDS('D'),
+    /** Represents Hearts Suit */
+    HEARTS('H'),
+    /** Represents Spades Suit */
+    SPADES('S');
+    /** A symbolic character to represent the Suit */
+    private char symbol;
+    
+    /** Creates a Suit with a given Symbol */
+    private Suit(char symbol) {
+        this.symbol = symbol;
+    }
+    
+    /** 
+     * Returns a String representing of this Suit. 
+     * @return a String with this Suit's symbol
+     */
+    @Override
+    public String toString() {
+        return Character.toString(this.symbol);
+    }
+}
